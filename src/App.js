e1645186021c18856d0e5bdc1e42e0480841d63f
GeoSauer <geoffrey.sauer89@gmail.com>
2023-02-07 18:54:21 -0700

    add auth form, header, and basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Auth from './components/Auth';
+import Auth from './components/Auth/Auth';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route path="/auth/:type" component={Auth} />
         <Route path="*">
